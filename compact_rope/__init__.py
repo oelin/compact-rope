39c5441dadbb9f36a4e8608c9e826b31239a433a
Kale Champagnie <42823429+oelin@users.noreply.github.com>
2024-05-19 10:46:25 +0100

    Create __init__.py

---
+++ compact_rope/__init__.py
@@ -0,0 +1,31 @@
+class RoPE(nn.Module):
+    def __init__(self, embedding_dimension: int) -> None:
+        super().__init__()
+
+        maximum = 10.
+
+        self.theta = torch.linspace(
+            start=math.log(0.5 * math.pi),
+            end=math.log(maximum * math.pi),
+            steps=embedding_dimension // 2,
+        ).exp().repeat_interleave(2, dim=-1)
+
+    def forward(self, x: torch.Tensor, position: torch.Tensor) -> torch.Tensor:
+        """Forward the module.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            The input tensor (..., T, E).
+        position : torch.Tensor
+            The position tensor (..., T).
+        """
+        
+        cos = torch.cos(position[..., None] * self.theta)
+        sin = torch.sin(position[..., None] * self.theta)
+
+        x_even, x_odd = x[..., :: 2], x[..., 1 :: 2]
+        x_right = torch.stack((-x_odd, x_even), dim=-1).view(x.shape)
+        x = x*cos + x_right*sin
+
+        return x
